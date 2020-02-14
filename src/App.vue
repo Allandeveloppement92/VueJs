e8864ef74eab3de6f407d121cf98a5882206973f
Allan Germé <55088102+Allandeveloppement92@users.noreply.github.com>
2020-02-14 21:17:37 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+      <img alt="Vue logo" src="./assets/Logo_vuejs.png">
+      <div class="lien">
+        <router-link to="/" id="link">Accueil</router-link> <!-- Activer la navigation utilisateur dans une app où le routeur est activé-->
+        <router-link to="/films" id="link">Nos Films</router-link>
+      </div>
+      <h1 style="color: rgb(10,126,131); font-weight:bold;">IIM Streaming</h1>
+      <router-view/> <!-- Permet fait le rendu des composants au dessus : https://router.vuejs.org/fr/api/#event-->
+      <Footer/>
+  </div>
+</template>
+
+
+
+<script>
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import Footer from'./components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    Footer,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  background-color:#9c9a9a;
+}
+
+.lien{
+  display: flex;
+  width: 200px;
+  justify-content: space-between;
+  transform: translateY(-250px);
+  margin-left: 1200px;
+  font-size: 20px;
+}
+
+#link:hover{
+  color: whitesmoke;
+  text-decoration: none;
+
+  cursor: pointer;
+}
+
+
+</style>
+
---
+++ src/views/Films.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1 style="margin-top:30px; margin-bottom:15px;">Nos films</h1>
+
+    <div v-for="film in info" :key="film.imbID" style="font-size:20px; margin-top:10px;">
+
+
+
+          
+               {{film.Title}}
+            {{film.Year}}
+            <img :src="film.Poster" alt="imgLoaded"/>
+          
+
+          
+          
+    </div>
+  </div>
+</template>
+
+<script>
+//import Test from'./../components/Test.vue'
+const axios = require('axios').default;
+
+export default {
+  name: 'HomePage',
+  components: {
+    // Test
+  },
+  props: {
+    message: String
+  },
+ data() {
+    return {
+            info:[],
+            };
+        },
+  
+  mounted() {
+    axios
+      .get('http://www.omdbapi.com/?s=batman&apikey=904eed5c')
+      .then(response => (this.info = response.data.Search))
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
